75ade025e6762d231abc47d93a5093ea9e86ca51
jinyoo28 <77855428+jinyoo28@users.noreply.github.com>
2021-01-29 12:41:07 -0500

    Add files via upload

---
+++ Week 3/Article.js
@@ -0,0 +1,29 @@
+// Import Component
+const Component = require("./Component.js");
+
+// Article is a child object of Component
+class Article extends Component {
+
+  constructor (props) {
+    // Send what it gets to its parent object
+     super(props);
+  }
+
+  render () {
+    // Return HTML with the title, author, and text of this article
+    // Use template literals for the variables
+    return (`
+      <Article>
+          <Title>${this.props.title}</Title>
+          <Author>${this.props.author}</Author>
+          <Text>${this.props.text}</Text>
+        </Article>
+      `
+    );
+
+  }
+
+}
+
+module.exports = Article;
+//exports the Article
