da87f1eb851d8374a46991c116e3738dc1378fe4
jinyoo28 <77855428+jinyoo28@users.noreply.github.com>
2021-03-22 18:49:30 -0400

    Add files via upload

---
+++ Week 11/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Card, Button} from 'react-native-elements';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Card>
+        <Card.Title>Yoo Jin Han</Card.Title>
+        <Text>Steak and Pasta</Text>
+        <Card.Divider/>
+        <Text>Strawberry Milk</Text>
+        <Button title="Submit"></Button>
+      </Card>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
