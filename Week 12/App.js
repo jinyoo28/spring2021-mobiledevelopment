45fa38e0fad7849c18da5b0d69d73d118984f615
jinyoo28 <77855428+jinyoo28@users.noreply.github.com>
2021-04-02 22:14:11 -0400

    Add files via upload

---
+++ Week 12/App.js
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements';
+
+export default function App() {
+  const [pet, setPet] = useState();
+
+  const [votes, setVotes] = useState(false);
+
+  function increaseCatVote(value){
+    setPet(value);
+    setVotes(true);
+  }
+
+  function increaseDogVote(value){
+    setPet(value);
+    setVotes(true);
+  }
+
+
+  return (
+    <View style={styles.container}>
+      { votes ? (
+      <View style={styles.container}>
+        <Text>You voted for {pet}!</Text>
+      </View>
+      ) : (
+        <View>
+        <Text style={styles.headerText}>Vote for your favorite pet!</Text>
+        <Card.Divider/>
+        <Card>
+          <Button title="Cats" onPress={(value) => (increaseCatVote('cats'))}/>
+        </Card>
+        <Card>
+          <Button title="Dogs" onPress={(value) => (increaseDogVote('dogs'))}/>
+        </Card>
+        </View>
+    )
+    }
+     </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    fontWeight: "bold"
+  }
+});
